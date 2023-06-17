cb9a6bd4d06565e7a639092646e3546e5a1c1ae1
Butterfly456 <122845909+Butterfly456@users.noreply.github.com>
2023-06-16 19:34:10 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
 var ironman;
 var ironmanImg;
+var fireball,fireballImg;
+
 
 function preload(){
     ironmanImg = loadImage("ironman.png");
+    fireballImg = loadImage("fireball.png");
+
 }
 
 function setup() {
@@ -24,5 +28,28 @@ function draw() {
     }
     if(keyDown("left_arrow")){
         ironman.x -= 5;
+    }
+
+    fireball = createSprite(ironman.x, ironman.y, 10,10);
+
+    if (keyDown("a")) {
+        fireball.addImage(fireballImg);
+        fireball.scale = 0.1;
+        fireball.velocityX = -6; 
+    }
+    if (keyDown("w")) {
+        fireball.addImage(fireballImg);
+        fireball.scale = 0.1;
+        fireball.velocityY = -6;  
+    }
+    if (keyDown("s")) {
+        fireball.addImage(fireballImg);
+        fireball.scale = 0.1;
+        fireball.velocityX = 6;  
+    }
+    if (keyDown("z")) {
+        fireball.addImage(fireballImg);
+        fireball.scale = 0.1;
+        fireball.velocityY = 6;
     }
 }
